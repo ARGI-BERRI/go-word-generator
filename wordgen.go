bc03ee49f83c7bc6ec355e00f757a520cab9fd83
ARGI-BERRI <me@endroit.net>
2025-02-01 00:44:25 +0900

    feat: add wordgen logics

---
+++ wordgen.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+type RandSource interface {
+	get(n int) int
+}
+
+type DefaultRandSource struct{}
+
+func (d DefaultRandSource) get(n int) int {
+	return rand.Intn(n)
+}
+
+func choiceSound(sounds []string, r RandSource) string {
+	if len(sounds) == 0 {
+		return ""
+	}
+
+	return sounds[r.get(len(sounds))]
+}
+
+func generateWords(config Config, randSource RandSource) []string {
+	sounds := config.Sounds
+	patterns := config.Pattern
+
+	var generatedWords []string
+
+	for _, pattern := range patterns {
+		fmt.Printf("Generating pattern: %s\n", pattern.Label)
+
+		var word string
+
+		for _, syllable := range pattern.Syllable {
+			if len(sounds[syllable]) == 0 {
+				fmt.Printf("'%s' is not defined in Sounds\n", syllable)
+				continue
+			}
+
+			word += choiceSound(sounds[syllable], randSource)
+		}
+
+		if len(word) > 0 {
+			generatedWords = append(generatedWords, word)
+		}
+	}
+
+	return generatedWords
+}
---
+++ wordgen_test.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+type FixedRandSource struct{}
+
+func (d FixedRandSource) get(_ int) int {
+	return 0
+}
+
+func Test_choiceSound(t *testing.T) {
+	tests := []struct {
+		name   string
+		sounds []string
+		want   string
+	}{
+		{
+			name:   "no sound is defined",
+			sounds: []string{},
+			want:   "",
+		},
+		{
+			name:   "only 1 sound is defined",
+			sounds: []string{"a"},
+			want:   "a",
+		},
+		{
+			name:   "some sounds are defined",
+			sounds: []string{"a", "b", "c"},
+			want:   "a",
+		},
+	}
+	for _, tt := range tests {
+
+		t.Run(tt.name, func(t *testing.T) {
+			if got := choiceSound(tt.sounds, FixedRandSource{}); got != tt.want {
+				t.Errorf("choiceSound() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_generateWords(t *testing.T) {
+	type args struct {
+		config Config
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{
+			name: "both syllables and sounds are defined",
+			args: args{
+				config: Config{
+					Sounds: map[string][]string{
+						"C": {"a", "c"},
+						"V": {"b", "d"},
+					},
+					Pattern: []Pattern{
+						{
+							Syllable: []string{
+								"C", "V",
+							},
+						},
+					},
+				},
+			},
+			want: []string{"ab"},
+		},
+		{
+			name: "patterns are not defined",
+			args: args{
+				config: Config{
+					Sounds: map[string][]string{
+						"C": {"a"},
+					},
+					Pattern: []Pattern{},
+				},
+			},
+			want: []string(nil),
+		},
+		{
+			name: "syllables refers to undefined sounds",
+			args: args{
+				config: Config{
+					Sounds: map[string][]string{
+						"C": {"a"},
+					},
+					Pattern: []Pattern{
+						{
+							Label: "Syllable that isn't defined in Sounds",
+							Syllable: []string{
+								"U",
+							},
+						},
+					},
+				},
+			},
+			want: []string(nil),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := generateWords(tt.args.config, FixedRandSource{}); !reflect.DeepEqual(got, tt.want) {
+				t.Logf("got: %#v, want: %#v", got, tt.want)
+				t.Errorf("generateWords() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}

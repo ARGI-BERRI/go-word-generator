77c86699f76b6a42fdfbad60e7e440bb1dd05c09
ARGI-BERRI <me@endroit.net>
2025-02-01 00:44:34 +0900

    feat: add wordgen to main

--- main.go
+++ main.go
@@ -15,4 +15,10 @@ func main() {
 	fmt.Println("INFO: Successfully loaded the config!")
 	fmt.Printf("INFO: %d sounds(s) are defined in the config.\n", len(config.Sounds))
 	fmt.Printf("INFO: %d pattern(s) are defined in the config.\n", len(config.Pattern))
+
+	words := generateWords(config, DefaultRandSource{})
+
+	for _, word := range words {
+		fmt.Println(word)
+	}
 }
